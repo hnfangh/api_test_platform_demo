172c4c9895f1f3ae4501f609578d6d5d652bc835
mac <hnfangh@gmail.com>
2023-07-07 19:00:50 +0800

    更新DO实体类代码

--- backend/do/__init__.py
+++ backend/do/__init__.py
@@ -1,4 +1,3 @@
-from server import db
 from do.user_entity import UserEntity   # 倒入要执行的实体类包
 
 """
@@ -6,5 +5,6 @@ from do.user_entity import UserEntity   # 倒入要执行的实体类包
     注意：创建一次后，就可以注释掉，避免之后每次启动项目都创建
 """
 
-if __name__ == "__main__":
-    db.create_all()
+# if __name__ == "__main__":
+#     db.create_all()
+    # 自动创建用户表 create_all()方法只会创建未被创建的表，而不会删除现有的表。
--- backend/do/user_entity.py
+++ backend/do/user_entity.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer
+from sqlalchemy import Integer, String
 from server import db
 
 
@@ -12,9 +12,9 @@ class UserEntity(db.Model):
     # 唯一主键ID
     id = db.Column(Integer,primary_key=True)
     # 用户名，唯一，不可为空
-    username = db.Column(db.String(64), nullable=False, unique=True)
+    username = db.Column(String(64), nullable=False, unique=True)
     # 密码，不为空
-    password = db.Column(db.String(128), nullable=False)
+    password = db.Column(String(128), nullable=False)
 
-    def user_entity_dict(self):
+    def as_user_entity_dict(self):
         return {"id": self.id, "username": self.username, "password": self.password}
