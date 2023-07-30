04ce27a7443372cd5d3194c8e33d16a87fa92eb0
mac <hnfangh@gmail.com>
2023-07-30 17:38:54 +0800

    新增testcase命名API的空间

--- backend/server.py
+++ backend/server.py
@@ -23,9 +23,11 @@ def add_router():
     from controller.user_controller import user_ns
     from controller.login_controller import login_ns
     from controller.register_controller import register_ns
+    from controller.testcase_controller import testcase_ns
     api.add_namespace(user_ns,"/user") # 添加api的命名空间，解决swagger不展示内容的问题
     api.add_namespace(login_ns, "/login")
     api.add_namespace(register_ns, "/register")
+    api.add_namespace(testcase_ns, "/testcase")
 
 
 if __name__ == "__main__":
